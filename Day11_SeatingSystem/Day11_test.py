20436dc324cc517b000187844900fd26a80317e5
Samuel Cordano <samuel.cordano@gmail.com>
2021-11-24 20:13:15 +0100

    Day 11: Added Test, test with entire test file

--- Day11_SeatingSystem/Day11_test.py
+++ Day11_SeatingSystem/Day11_test.py
@@ -15,3 +15,19 @@ def test_next_state_1():
     max_row = int(len(current_layout))-1
     max_column = int(len(current_layout[0]))-1
     assert "." == Day11.next_state(current_layout,1,1,max_row,max_column)
+
+
+
+def test_q1():
+    inputFile = open("Day11_SeatingSystem/InputTest.txt","r")
+    Lines = inputFile.readlines()
+
+    input_list = []
+    for line in Lines:
+        input_line = []
+        currentInput = line.strip()
+        for character in currentInput:
+            input_line.append(character)
+        input_list.append(input_line)
+    
+    assert 37 == Day11.q1(input_list)
